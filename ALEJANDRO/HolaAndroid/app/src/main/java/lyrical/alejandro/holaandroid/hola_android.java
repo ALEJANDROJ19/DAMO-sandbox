1d2bb9d7fcce568e066f7261f2e3ac95f49a9be7
ALEJANDROJ19 <ALEJANDROJ19@hotmail.es>
2016-09-06 13:55:57 +0200

    HolaAndroid first
    Primer dia de classe

---
+++ ALEJANDRO/HolaAndroid/app/src/main/java/lyrical/alejandro/holaandroid/hola_android.java
@@ -0,0 +1,13 @@
+package lyrical.alejandro.holaandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class hola_android extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hola_android);
+    }
+}
